37e56adc2473c617cce8cb859c9b2d4bd38338b9
Raul <bluepit@yahoo.com>
2013-04-25 22:30:51 +0100

    upload

--- Cinema/src/cinema/Booking.java
+++ Cinema/src/cinema/Booking.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  */
 public class Booking implements Serializable {  
 	private static final long serialVersionUID = -7266881288297174414L;
-	Showing show;
+	Showing show; 
 	int number; 
 	int bookingId;
 	private Person p;
